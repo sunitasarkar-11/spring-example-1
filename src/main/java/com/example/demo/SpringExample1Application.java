a8b5507b2d74d6f0f2ebf05ee6a77e4acf9fa3a0
sunitasarkar-11 <58844360+sunitasarkar-11@users.noreply.github.com>
2019-12-13 16:51:07 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/SpringExample1Application.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringExample1Application {
+
+	
+	@GetMapping("/")		
+	public String welcome(){
+		return "Welcome to Sunita for Openshift PoC";	}
+	@GetMapping("/{input}")		
+	public String congrats(@PathVariable String input){		return "Welcome "+input +"to Sunita for Openshift PoC";	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExample1Application.class, args);
+	}
+
+}
